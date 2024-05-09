d340aca9c8ce01cbeca3a0e876b164360509045f
Nikita Potashev <potashev.nik@gmail.com>
2024-05-09 20:30:26 +0700

    Add project files.

---
+++ DynamicPriceService/Data/DynamicPriceServiceContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DynamicPriceService.Models;
+
+namespace DynamicPriceService.Data;
+public class DynamicPriceServiceContext : DbContext
+{
+    public DynamicPriceServiceContext (DbContextOptions<DynamicPriceServiceContext> options)
+        : base(options)
+    {
+		//todo: for development - remove
+		//Database.EnsureDeleted();
+		Database.EnsureCreated();
+	}
+
+	public DbSet<Company> Company { get; set; } = default!;
+	public DbSet<Product> Product { get; set; } = default!;
+    public DbSet<PriceRule> PriceRule { get; set; } = default!;
+}
---
+++ DynamicPriceService/Models/PriceRule.cs
@@ -0,0 +1,25 @@
+﻿namespace DynamicPriceService.Models;
+
+/// <summary>
+/// Правило изменения цены для компании.
+/// </summary>
+public class PriceRule
+{
+	public int PriceRuleId { get; set; }
+	public Company Company { get; set; }
+
+	/// <summary>
+	/// Повышение цены продукта (в %)
+	/// </summary>
+	public int Increase { get; set; }
+
+	/// <summary>
+	/// Снижение цены продукта (в %)
+	/// </summary>
+	public int Reduction { get; set; }
+
+	/// <summary>
+	/// Допустимое время "простоя" продукта. Если превысили - снижаем цену (см. ReducePriceService).
+	/// </summary>
+	public TimeSpan? NoSellTime { get; set; }
+}

944b1a3011724dba0e5511f64f22c3296f38aedd
Nikita Potashev <potashev.nik@gmail.com>
2024-05-16 17:55:31 +0700

    Added Quartz job scheduling for price reducing.

--- DynamicPriceService/Program.cs
+++ DynamicPriceService/Program.cs
@@ -2,16 +2,20 @@
 using Microsoft.Extensions.DependencyInjection;
 using DynamicPriceService.Data;
 using DynamicPriceService.Services;
-using Microsoft.Extensions.Hosting;
+using Quartz;
+using DynamicPriceService.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DynamicPriceServiceContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DynamicPriceServiceContext") ?? throw new InvalidOperationException("Connection string 'DynamicPriceServiceContext' not found.")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DynamicPriceServiceContext") ?? throw new InvalidOperationException("Connection string 'DynamicPriceServiceContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSingleton<IActiveCompaniesService, ActiveCompaniesService>();	//singleton - is it right?
-builder.Services.AddHostedService<ReducePriceService>();
+builder.Services.AddSingleton<IActiveCompaniesService, ActiveCompaniesService>();   //singleton - is it right?
+
+builder.Services.AddQuartz(q => q.AddJobAndTrigger<ReducePriceJob>(builder.Configuration));
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
 var app = builder.Build();
 

6e44a81cdbb90eb6246698fc5760168621121ecb
Potashev <potashev.nik@gmail.com>
2025-09-12 18:04:33 +0700

    Implemented base drawing.

---
+++ DynamicPrice.Client.Drawing/PriceHubClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace DynamicPrice.Client.Drawing;
+
+public class PriceHubClientOptions
+{
+	public string HubUrl { get; set; } = "/priceHub";
+}
+
+public static class PriceHubClientExtensions
+{
+	//public static IApplicationBuilder UsePriceMonitorStaticFiles(this IApplicationBuilder app)
+	//{
+	//	// Подключаем wwwroot из общей библиотеки
+	//	app.UseStaticFiles(new StaticFileOptions
+	//	{
+	//		FileProvider = new ManifestEmbeddedFileProvider(typeof(PriceHubClientExtensions).Assembly, "wwwroot"),
+	//		RequestPath = ""
+	//	});
+
+	//	return app;
+	//}
+}
---
+++ DynamicPrice.Client.Drawing/wwwroot/js/price-monitor.js
@@ -0,0 +1,68 @@
+﻿export class PriceMonitor {
+	constructor(hubUrl) {
+		this.hubUrl = hubUrl;
+		this.connection = null;
+		this.charts = {};
+	}
+
+	async init() {
+		this.connection = new signalR.HubConnectionBuilder()
+			.withUrl(this.hubUrl)
+			.build();
+
+		await this.connection.start();
+
+		console.log("[PriceMonitor] Connected to hub");
+		this.connection.on("ReceivePriceUpdate", (productId, newPrice) => {
+			this.updateChart(productId, newPrice);
+		});
+	}
+
+	registerChart(productId, canvasId, initialData = []) {
+		const ctx = document.getElementById(canvasId).getContext('2d');
+		const chart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: initialData.map((_, i) => `T${i + 1}`),
+				datasets: [{
+					label: 'Price Change',
+					data: initialData,
+					borderColor: 'rgba(75, 192, 192, 1)',
+					borderWidth: 2,
+					fill: true,
+					pointRadius: 1
+				}]
+			},
+			options: {
+				scales: {
+					x: { display: false },
+					y: { display: false }
+				},
+				plugins: {
+					legend: { display: false }
+				},
+				animation: {
+					duration: 300,
+					easing: 'linear'
+				}
+			}
+		});
+
+		this.charts[productId] = chart;
+	}
+
+	updateChart(productId, newPrice) {
+		const chart = this.charts[productId];
+		if (!chart) return;
+
+		chart.data.datasets[0].data.push(newPrice);
+		chart.data.labels.push(new Date().toLocaleTimeString());
+
+		if (chart.data.datasets[0].data.length > 100) {
+			chart.data.datasets[0].data.shift();
+			chart.data.labels.shift();
+		}
+
+		chart.update();
+	}
+}

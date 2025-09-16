9acc8e68e9c033d7fd3c072b5c50e287bdbbf286
Potashev <potashev.nik@gmail.com>
2025-09-16 15:44:57 +0700

    Updated customer view with taghelpers.

--- DynamicPriceClient/ViewModels/ProductInfoViewModel.cs
+++ DynamicPriceClient/ViewModels/ProductInfoViewModel.cs
@@ -12,7 +12,7 @@ public class ProductInfoViewModel
 
 	[Display(Name = "Product")]
 	public string Title { get; set; }
-	public double Price { get; set; }
+	public decimal Price { get; set; }
 	public int? Quantity { get; set; }
 	public string? Description { get; set; }
 

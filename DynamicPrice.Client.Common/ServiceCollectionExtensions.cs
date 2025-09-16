9724f324e0a715a095bf36dd095b12d94cea5974
Potashev <potashev.nik@gmail.com>
2025-09-16 17:50:35 +0700

    Minor fixes.

--- DynamicPrice.Client.Common/ServiceCollectionExtensions.cs
+++ DynamicPrice.Client.Common/ServiceCollectionExtensions.cs
@@ -8,7 +8,6 @@ public static class ServiceCollectionExtensions
 	public static IServiceCollection AddClientCommon(this IServiceCollection services)
 	{
 		services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-		//services.TryAddScoped<IAuthTokenStore, SessionAuthTokenStore>();
 		services.TryAddScoped<IAuthTokenStore, CookiesAuthTokenStore>();
 		services.AddTransient<AuthHeaderHandler>();
 

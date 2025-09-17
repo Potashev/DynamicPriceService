dbe9b89a6ebbc7cb28b875e5247802a672b069e4
Potashev <potashev.nik@gmail.com>
2025-09-17 11:37:20 +0700

    Added colors for order status in views.

---
+++ DynamicPrice.Client.Common/TagHelpers/OrderStatusTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using DynamicPrice.Client.Common;
+
+namespace DynamicPrice.Client.TagHelpers
+{
+	[HtmlTargetElement("order-status")]
+	public class OrderStatusTagHelper : TagHelper
+	{
+		public OrderStatus Status { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "span";
+
+			string cssClass = Status switch
+			{
+				OrderStatus.Completed => "text-success",
+				OrderStatus.Canceled => "text-danger",
+				_ => ""
+			};
+
+			if (!string.IsNullOrEmpty(cssClass))
+			{
+				output.Attributes.SetAttribute("class", cssClass);
+			}
+
+			output.Content.SetContent(Status.ToString());
+		}
+	}
+}

a74e0d245c71376169e847962c2b96f3557231af
Potashev <potashev.nik@gmail.com>
2025-11-26 13:51:10 +0700

    Minor fixes.

--- DynamicPriceCore/Services/Events.cs
+++ DynamicPriceCore/Services/Events.cs
@@ -2,13 +2,5 @@
 
 namespace DynamicPrice.Core.Services;
 
-public record CompanyMonitoringEvent(int CompanyId, MonitoringEvent Event);
 public record PriceReduceEvent(int ProductId);
-//public record PriceIncreaseEvent(int OrderId);
 public record PriceIncreaseEvent(ICollection<OrderItem> OrderItems);
-
-public enum MonitoringEvent
-{
-    Start,
-    Stop
-}

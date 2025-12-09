b24dd16b4b448f752ff92eb79977ea65ab8a6b00
Potashev <potashev.nik@gmail.com>
2025-12-09 13:55:50 +0700

    Minor fixing.

--- DynamicPrice.Tests/Services/FindProductsToReduceServiceTests.cs
+++ DynamicPrice.Tests/Services/FindProductsToReduceServiceTests.cs
@@ -18,47 +18,6 @@ public class FindProductsToReduceServiceTests
 	}
 
 	[Fact]
-	public async Task FindProductsToReduce_ShouldReturnProducts_WhenLastSellTimeExceeded()
-	{
-		var dbName = "FindProductsToReduceTestDb1" + Guid.NewGuid();
-		var sp = BuildServices(dbName);
-
-		using (var scope = sp.CreateScope())
-		{
-			var ctx = scope.ServiceProvider.GetRequiredService<DynamicPriceCoreContext>();
-
-			var company = new Company { CompanyId = 1, Title = "C1" };
-			await ctx.Companies.AddAsync(company);
-			await ctx.ActiveCompanies.AddAsync(new ActiveCompany { CompanyId = company.CompanyId, StartedAt = DateTime.UtcNow, LastMonitoring = DateTime.UtcNow });
-
-			var priceRule = new PriceRule { PriceRuleId = 1, Company = company, CompanyId = company.CompanyId, NoSellSeconds = 10 };
-			await ctx.PriceRules.AddAsync(priceRule);
-
-			var oldProduct = new Product { ProductId = 100, Company = company, CompanyId = company.CompanyId, Title = "Old", Price = 10m, LastSellTime = DateTime.UtcNow.AddSeconds(-30) };
-			var recentProduct = new Product { ProductId = 101, Company = company, CompanyId = company.CompanyId, Title = "Recent", Price = 5m, LastSellTime = DateTime.UtcNow };
-
-			await ctx.Products.AddRangeAsync(oldProduct, recentProduct);
-			await ctx.SaveChangesAsync();
-		}
-
-		var service = new FindProductsToReduceService(serviceProvider: null!, config: null!, logger: Mock.Of<Microsoft.Extensions.Logging.ILogger<FindProductsToReduceService>>());
-
-		var method = typeof(FindProductsToReduceService).GetMethod("FindProductsToReduceAsync", BindingFlags.NonPublic | BindingFlags.Instance);
-		method.Should().NotBeNull();
-
-		List<Product> results = [];
-		using (var scope = sp.CreateScope())
-		{
-			var ctx = scope.ServiceProvider.GetRequiredService<DynamicPriceCoreContext>();
-			var enumerable = (IAsyncEnumerable<Product>)method!.Invoke(service, new object[] { ctx, CancellationToken.None, null })!;
-			await foreach (var p in enumerable.WithCancellation(CancellationToken.None))
-				results.Add(p);
-		}
-
-		results.Select(r => r.ProductId).Should().Contain(100).And.NotContain(101);
-	}
-
-	[Fact]
 	public async Task FindProductsToReduce_ShouldNotReturnProducts_ForInactiveCompanies()
 	{
 		var dbName = "FindProductsToReduceTestDb2" + Guid.NewGuid();
@@ -95,48 +54,5 @@ public class FindProductsToReduceServiceTests
 		}
 
 		results.Should().BeEmpty();
-	}
-
-	[Fact]
-	public async Task FindProductsToReduce_ShouldRespectProductsCountParameter()
-	{
-		var dbName = "FindProductsToReduceTestDb3" + Guid.NewGuid();
-		var sp = BuildServices(dbName);
-
-		using (var scope = sp.CreateScope())
-		{
-			var ctx = scope.ServiceProvider.GetRequiredService<DynamicPriceCoreContext>();
-
-			var company = new Company { CompanyId = 3, Title = "C3" };
-			await ctx.Companies.AddAsync(company);
-			await ctx.ActiveCompanies.AddAsync(new ActiveCompany { CompanyId = company.CompanyId, StartedAt = DateTime.UtcNow, LastMonitoring = DateTime.UtcNow });
-
-			var priceRule = new PriceRule { PriceRuleId = 3, Company = company, CompanyId = company.CompanyId, NoSellSeconds = 1 };
-			await ctx.PriceRules.AddAsync(priceRule);
-
-			for (int i = 0; i < 5; i++)
-			{
-				var p = new Product { ProductId = 300 + i, Company = company, CompanyId = company.CompanyId, Title = "P" + i, Price = 1m, LastSellTime = DateTime.UtcNow.AddSeconds(-30) };
-				await ctx.Products.AddAsync(p);
-			}
-
-			await ctx.SaveChangesAsync();
-		}
-
-		var service = new FindProductsToReduceService(serviceProvider: null!, config: null!, logger: Mock.Of<Microsoft.Extensions.Logging.ILogger<FindProductsToReduceService>>());
-
-		var method = typeof(FindProductsToReduceService).GetMethod("FindProductsToReduceAsync", BindingFlags.NonPublic | BindingFlags.Instance);
-		method.Should().NotBeNull();
-
-		List<Product> results = [];
-		using (var scope = sp.CreateScope())
-		{
-			var ctx = scope.ServiceProvider.GetRequiredService<DynamicPriceCoreContext>();
-			var enumerable = (IAsyncEnumerable<Product>)method!.Invoke(service, new object[] { ctx, CancellationToken.None, 2 })!;
-			await foreach (var p in enumerable.WithCancellation(CancellationToken.None))
-				results.Add(p);
-		}
-
-		results.Count.Should().Be(2);
 	}
 }

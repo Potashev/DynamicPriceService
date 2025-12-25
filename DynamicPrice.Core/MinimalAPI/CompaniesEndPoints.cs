3ff73c47f285cee2a47d34bea6e689b96d937bb4
Potashev <potashev.nik@gmail.com>
2025-12-25 13:51:27 +0700

    Added CompaniesEndPoints.

---
+++ DynamicPrice.Core/MinimalAPI/CompaniesEndPoints.cs
@@ -0,0 +1,36 @@
+﻿using DynamicPrice.Core.MediatR.CompanyEntity.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicPrice.Core.MinimalAPI;
+
+public static class CompaniesEndPoints
+{
+	public static void MapCompaniesEndPoints(this IEndpointRouteBuilder app)
+	{
+		var companies = app.MapGroup("/api/companies")
+			.RequireAuthorization("CustomerPolicy");
+
+		companies.MapGet("/", GetCompanies)
+			.WithSummary("Get all products");
+		companies.MapGet("/{companyId}/products", GetCompanyProducts)
+			.WithSummary("Get a product by id");
+	}
+
+	private static async Task<IResult> GetCompanies([FromQuery] string? status, IMediator mediator, CancellationToken cancellationToken)
+	{
+		if (status == "active")
+		{
+			var activeCompanies = await mediator.Send(new GetActiveCompaniesQuery(), cancellationToken);
+			return Results.Ok(activeCompanies);
+		}
+
+		return Results.StatusCode(StatusCodes.Status501NotImplemented); //todo: handle
+	}
+
+	private static async Task<IResult> GetCompanyProducts(string companyId, IMediator mediator, CancellationToken cancellationToken)
+	{
+		var companyProducts = await mediator.Send(new GetCompanyProductsQuery(companyId), cancellationToken);
+		return Results.Ok(companyProducts);
+	}
+}

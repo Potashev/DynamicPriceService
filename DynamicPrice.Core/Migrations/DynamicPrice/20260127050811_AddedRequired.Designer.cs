f20f6bd9585dccb6e5ef89befdb379113bfe94f9
Potashev <potashev.nik@gmail.com>
2026-01-27 12:13:28 +0700

    Added migration.

--- DynamicPrice.Core/Migrations/DynamicPrice/20260127050811_AddedRequired.Designer.cs
+++ DynamicPrice.Core/Migrations/DynamicPrice/20260127050811_AddedRequired.Designer.cs
@@ -4,6 +4,7 @@ using DynamicPrice.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DynamicPrice.Core.Migrations.DynamicPrice
 {
     [DbContext(typeof(DynamicPriceCoreContext))]
-    partial class DynamicPriceCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260127050811_AddedRequired")]
+    partial class AddedRequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

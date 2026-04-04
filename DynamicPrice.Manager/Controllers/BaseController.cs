4db5aadc49a873b6b84ec7e0414bb09aee1c7249
Potashev <potashev.nik@gmail.com>
2026-04-04 17:03:38 +0700

    Minor fixes.

--- DynamicPrice.Manager/Controllers/BaseController.cs
+++ DynamicPrice.Manager/Controllers/BaseController.cs
@@ -1,8 +1,7 @@
 ﻿using DynamicPrice.Manager.ApiClients;
-using DynamicPrice.Manager.Extension;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System.Net;
+using Refit;
 
 namespace DynamicPrice.Manager.Controllers;
 
@@ -24,59 +23,16 @@ public class BaseController : Controller
 			var manager = await CoreApiClient.GetManager();
 			ViewData["ManagerId"] = manager.Id;
 			ViewData["CompanyTitle"] = manager.Company.Title;
-
-			var executedContext = await next();
-
-			if (executedContext.Exception is Refit.ApiException apiEx)
-			{
-				executedContext.ExceptionHandled = true;
-				context.Result = await HandleApiException(apiEx);
-			}
 		}
-		catch (Refit.ApiException ex)
+		catch (ApiException ex)
 		{
-			context.Result = await HandleApiException(ex);
+			// можно игнорить или логировать
 		}
-		catch (Exception)
+		catch (Exception ex)
 		{
-			context.Result = View("Error");
-		}
-	}
-
-	private async Task<IActionResult> HandleApiException(Refit.ApiException ex)
-	{
-		var problem = await ex.GetProblemDetailsAsync();
-
-		return ex.StatusCode switch
-		{
-			HttpStatusCode.BadRequest => HandleBadRequest(problem),
-			HttpStatusCode.NotFound => NotFound(),
-			HttpStatusCode.Unauthorized => RedirectToAction("Login", "Auth"),
-			_ => View("Error", problem)
-		};
-	}
-
-	private IActionResult HandleBadRequest(ProblemDetails? problem)
-	{
-		if (problem is ValidationProblemDetails validation)
-		{
-			foreach (var (key, errors) in validation.Errors)
-			{
-				foreach (var error in errors)
-				{
-					ModelState.AddModelError(key, error);
-				}
-			}
-		}
-		else if (problem != null)
-		{
-			ModelState.AddModelError("", problem.Detail);
+			// логировать обязательно
 		}
 
-		//// ⚠️ вот тут нюанс (см. ниже)
-		//return View();
-
-		var actionName = ControllerContext.ActionDescriptor.ActionName;
-		return View(actionName);
+		await next();
 	}
 }
